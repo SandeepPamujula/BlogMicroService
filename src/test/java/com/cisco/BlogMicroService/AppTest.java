522061e8dcecb60a8c3733b307523dc0117143d4
Sandeep Kumar Pamujula <sandeek5@cisco.com>
2016-06-23 20:06:27 +0530

    Blog test cases

--- src/test/java/com/cisco/BlogMicroService/AppTest.java
+++ src/test/java/com/cisco/BlogMicroService/AppTest.java
@@ -1,38 +1,69 @@
 package com.cisco.BlogMicroService;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+//import junit.framework.Test;
+//import junit.framework.TestCase;
+//import junit.framework.TestSuite;
 
-/**
- * Unit test for simple App.
- */
-public class AppTest 
-    extends TestCase
-{
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import com.hazelcast.util.Base64;
+
+import io.vertx.core.Vertx;
+import io.vertx.core.VertxOptions;
+import io.vertx.ext.unit.TestContext;
+import io.vertx.ext.unit.junit.VertxUnitRunner;
+
+import static com.jayway.restassured.RestAssured.*;
+//import org.apache.commons.codec.binary.Base64;
+//import java.util.Base64;
+@RunWith(VertxUnitRunner.class)
+public class AppTest {
+	
+	String baseURL = "http://localhost:8082";
+	String userName = "sand";
+	String password = "pass";
+	Vertx vertx;
+	
+	
+	@Before
+    public void before(TestContext context) {
+		VertxOptions options = new VertxOptions().setWorkerPoolSize(10);
+		vertx = Vertx.vertx(options);
+		vertx.deployVerticle(BlogMicroVerticle.class.getName(), stringAsyncResult -> {
+			System.out.println(BlogMicroVerticle.class.getName() + "Deployment Completed");
+		});
+    }
+	@After
+    public void after(TestContext context) {
+        vertx.close(context.asyncAssertSuccess());
     }
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
-    }
-
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
-    }
+//	@Test
+	public void test001_validateBlogsGet() {
+		
+		String url = baseURL + "/Services/rest/blogs";
+		System.out.println(url);
+		given().
+		when().
+			get(url).
+		then().
+			statusCode(200);
+//			body("[0].id", equalTo("55716669eec5ca2b6ddf5626")).
+//			body("[1].id", equalTo("559e4331c203b4638a00ba1a"));
+	}
+//	@Test
+//	public void test002_validatePostNewBlog(){
+//		
+//		String url = baseURL + "/Services/rest/blogs";
+//		String headers = "sande:pass:sandeep:pamujlua:{\"timestamp\":1462608539,\"machineIdentifier\":820102,\"processIdentifier\":19386,\"counter\":4621142,\"timeSecond\":1462608539,\"time\":1462608539000,\"date\":1462608539000}\"";
+//		given()
+//			.when()
+//				.header("Authorization", "Basic"+Base64.encode(headers.getBytes()))
+//				.post(url)
+//			.then()
+//				.statusCode(204);
+//	}
 }
